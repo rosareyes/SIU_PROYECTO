6f1b5459982da8249fc44e0b5cdcb9d4866cc699
Rosa Reyes <rosaareyesc@gmail.com>
2024-04-17 16:15:44 +0200

    carrito

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
   // una vez el DOM este cargado se ejecuta el script
+  const addToCartBtn = document.getElementById('add-to-cart-btn');
+
+  addToCartBtn.addEventListener('click', () => {
+    const producto = JSON.parse(localStorage.getItem('producto'));
+
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+    const existingProduct = carrito.findIndex((p) => p.id === producto.id);
+
+    if (existingProduct !== -1) {
+      carrito[existingProduct].cantidad =
+        (carrito[existingProduct].cantidad || 1) + 1;
+    } else {
+      producto.cantidad = 1;
+      carrito.push(producto);
+    }
+
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+
+    // Redireccionar a la página del carrito
+    window.location.href = 'cart.html';
+  });
+
   const producto = JSON.parse(localStorage.getItem('producto'));
   if (producto) {
     const container = document.getElementById('product-info-container');
