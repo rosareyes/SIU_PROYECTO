19039302f3019df4ac263f369527e95f6a532afc
Rosa Reyes <rosaareyesc@gmail.com>
2024-04-18 22:08:58 +0200

    Add route for homepage

--- index.js
+++ index.js
@@ -30,15 +30,25 @@ io.on('connection', (socket) => {
   });
 });
 
+app.get('/', (req, res) => {
+  const htmlResponse = `
+    <html>
+      <head>
+        <title>NodeJs y Express en Vercel</title>
+      </head>
+      <body>
+        <h1>Soy un proyecto Back end en vercel</h1>
+      </body>
+    </html>
+  `;
+  res.send(htmlResponse);
+});
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 // Serve static files from 'www' directory
 app.use(express.static('www'));
-
-app.get('/', (req, res) => {
-  res.sendFile('www/index.html', { root: 'www' });
-});
 
 app.get('/cart', async (req, res) => {
   try {
