a26ae61404d1efc9c504100f89458f147f70cc07
Rosa Reyes <rosaareyesc@gmail.com>
2024-04-18 22:11:34 +0200

    Refactor HTML response to use index.html file

--- index.js
+++ index.js
@@ -31,17 +31,7 @@ io.on('connection', (socket) => {
 });
 
 app.get('/', (req, res) => {
-  const htmlResponse = `
-    <html>
-      <head>
-        <title>NodeJs y Express en Vercel</title>
-      </head>
-      <body>
-        <h1>Soy un proyecto Back end en vercel</h1>
-      </body>
-    </html>
-  `;
-  res.send(htmlResponse);
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 // Middleware to parse JSON bodies
